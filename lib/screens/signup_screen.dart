41b564771a99804d3294837c095d7a6c381af382
Sherange <sherangef@gmail.com>
2022-05-20 13:33:43 +0530

    signup form validation

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -15,6 +15,8 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
@@ -25,7 +27,6 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
@@ -34,31 +35,38 @@ class _SignupScreenState extends State<SignupScreen> {
   }
 
   void selectImage() async {
-    Uint8List imageFile = await pickImage(ImageSource.gallery);
+    Uint8List imageFile = await pickImage(ImageSource.camera);
     setState(() {
       _image = imageFile;
     });
   }
 
   void signUpUser() async {
-    setState(() {
-      _isLoading = true;
-    });
+    if (_formKey.currentState!.validate()) {
+      if (_image == null) {
+        showSnackBar("Profile photo is required", context);
+        return;
+      }
 
-    String res = await AuthMethods().signUpUser(
-        email: _emailController.text,
-        password: _passwordController.text,
-        username: _usernameController.text,
-        bio: _bioController.text,
-        file: _image!);
+      setState(() {
+        _isLoading = true;
+      });
 
-    if (res != "sucess") {
-      showSnackBar(res, context);
+      String res = await AuthMethods().signUpUser(
+          email: _emailController.text,
+          password: _passwordController.text,
+          username: _usernameController.text,
+          bio: _bioController.text,
+          file: _image!);
+
+      if (res != "sucess") {
+        showSnackBar(res, context);
+      }
+
+      setState(() {
+        _isLoading = false;
+      });
     }
-
-    setState(() {
-      _isLoading = false;
-    });
   }
 
   @override
@@ -68,135 +76,162 @@ class _SignupScreenState extends State<SignupScreen> {
           child: SingleChildScrollView(
               // width: double.infinity,
               padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  // svg image
-                  Image.asset(
-                    "assets/images/Insta.png",
-                    height: 150,
-                    fit: BoxFit.contain,
-                    // color: Colors.amber,
-                  ),
-                  // circular avatar
-                  Stack(
-                    children: [
-                      _image != null
-                          ? CircleAvatar(
-                              radius: 64,
-                              backgroundImage: MemoryImage(_image!),
-                            )
-                          : const CircleAvatar(
-                              radius: 64,
-                              backgroundImage:
-                                  AssetImage("assets/images/profile_pic.jpg"),
-                            ),
-                      Positioned(
-                        child: IconButton(
-                          onPressed: selectImage,
-                          icon: const Icon(Icons.add_a_photo),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    // svg image
+                    Image.asset(
+                      "assets/images/Insta.png",
+                      height: 150,
+                      fit: BoxFit.contain,
+                      // color: Colors.amber,
+                    ),
+                    // circular avatar
+                    Stack(
+                      children: [
+                        _image != null
+                            ? CircleAvatar(
+                                radius: 64,
+                                backgroundImage: MemoryImage(_image!),
+                              )
+                            : const CircleAvatar(
+                                radius: 64,
+                                backgroundImage:
+                                    AssetImage("assets/images/profile_pic.jpg"),
+                              ),
+                        Positioned(
+                          child: IconButton(
+                            onPressed: selectImage,
+                            icon: const Icon(Icons.add_a_photo),
+                          ),
+                          bottom: -10,
+                          left: 80,
                         ),
-                        bottom: -10,
-                        left: 80,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // text field username
-                  TextFieldInput(
-                    textEditingController: _usernameController,
-                    hintText: "Enter your username",
-                    textInputType: TextInputType.text,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // text field email
-                  TextFieldInput(
-                    textEditingController: _emailController,
-                    hintText: "Enter your email",
-                    textInputType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // text field password
-                  TextFieldInput(
-                    textEditingController: _passwordController,
-                    hintText: "Enter your password",
-                    textInputType: TextInputType.text,
-                    isPass: true,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // text field bio
-                  TextFieldInput(
-                    textEditingController: _bioController,
-                    hintText: "Enter your bio",
-                    textInputType: TextInputType.text,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: signUpUser,
-                    child: Container(
-                      child: _isLoading
-                          ? const SizedBox(
-                              height: 20.0,
-                              width: 20.0,
-                              child: Center(
-                                child: CircularProgressIndicator(
-                                  color: primaryColor,
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // text field username
+                    TextFieldInput(
+                      textEditingController: _usernameController,
+                      hintText: "Enter your username",
+                      textInputType: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'User name is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // text field email
+                    TextFieldInput(
+                      textEditingController: _emailController,
+                      hintText: "Enter your email",
+                      textInputType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'User email is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // text field password
+                    TextFieldInput(
+                      textEditingController: _passwordController,
+                      hintText: "Enter your password",
+                      textInputType: TextInputType.text,
+                      isPass: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // text field bio
+                    TextFieldInput(
+                      textEditingController: _bioController,
+                      hintText: "Enter your bio",
+                      textInputType: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'User name is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: signUpUser,
+                      child: Container(
+                        child: _isLoading
+                            ? const SizedBox(
+                                height: 20.0,
+                                width: 20.0,
+                                child: Center(
+                                  child: CircularProgressIndicator(
+                                    color: primaryColor,
+                                  ),
+                                ),
+                              )
+                            : const SizedBox(
+                                height: 20.0,
+                                width: 100.0,
+                                child: Center(
+                                  child: Text("Sign up"),
                                 ),
                               ),
-                            )
-                          : const SizedBox(
-                              height: 20.0,
-                              width: 100.0,
-                              child: Center(
-                                child: Text("Sign up"),
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: const ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(4),
                               ),
                             ),
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      decoration: const ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(4),
+                            color: blueColor),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: const Text("Dont have an account?  "),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: const Text(
+                              "Sign up",
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
                           ),
-                          color: blueColor),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: const Text("Dont have an account?  "),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: const Text(
-                            "Sign up",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ))),
     );
   }
--- lib/widgets/text_field_input.dart
+++ lib/widgets/text_field_input.dart
@@ -5,20 +5,24 @@ class TextFieldInput extends StatelessWidget {
   final bool isPass;
   final String hintText;
   final TextInputType textInputType;
+  final String? Function(String?)? validator;
+
   const TextFieldInput(
       {Key? key,
       required this.textEditingController,
       this.isPass = false,
       required this.hintText,
-      required this.textInputType})
+      required this.textInputType,
+      this.validator})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final inputBorder =
         OutlineInputBorder(borderSide: Divider.createBorderSide(context));
-    return TextField(
+    return TextFormField(
       controller: textEditingController,
+      validator: validator,
       decoration: InputDecoration(
         hintText: hintText,
         border: inputBorder,
